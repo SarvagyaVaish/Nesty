0667da755531f98fc9187ed38c9eac1a277abcb1
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2014-09-04 19:15:43 -0400

    - Checking core online / offline state by fetching the current temp instead of making an extra IsOnline call. - Better Hammer Js: using single and double taps.

--- Nesty.js
+++ Nesty.js
@@ -9,76 +9,19 @@ $(function () {
 	DropboxDB.Init();
 
 	// Set up ping to spark core (every 5 seconds)
-	IsCoreOnline();
-	setInterval(IsCoreOnline, 5000);
+	GetCurrentTemperature();
+	setInterval(GetCurrentTemperature, 5000);
 
 	// HAMMER.JS
-	var tapArea = $('#tap-area')[0];
-	var mc = new Hammer(tapArea);
-	mc.on("tap", function(ev) {
-		if (ThermostatUI.GetThermostatMode() == 'current-temp') {
-			DebugLog('Tap gesture recognized.', 2);
-			ThermostatUI.SetThermostatMode("desired-temp");
-		}
+	var tapArea = document.getElementById('tap-area');
+	var mc = new Hammer.Manager(tapArea);
+	mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
+	mc.add( new Hammer.Tap({ event: 'singletap' }) );
+	mc.get('doubletap').recognizeWith('singletap');
+	mc.get('singletap').requireFailure('doubletap');
+	mc.on("singletap doubletap", function(ev) {
+	    console.log(ev.type + " ");
 	});
-
-	function CorePingSuccessful(data) {
-		var response = data[0]	;
-		if ('last_heard' in response) {
-			var lastHeardTime = response.last_heard;
-
-			/*
-			var d = new Date();
-			var n = d.toUTCString();
-			DebugLog(n);
-
-			var d2 = new Date(Date.parse(lastHeardTime));
-			var n2 = d2.toUTCString();
-			DebugLog(n2);
-			*/
-
-			if (Date.now() - Date.parse(lastHeardTime) < 30000) {
-				if (ThermostatUI.GetOnline()) {
-					DebugLog("[Core Ping Successful] Core Already Online.", 2);
-				}
-				else {
-					DebugLog("[Core Ping Successful] Core Online.", 2);
-					ThermostatUI.SetOnline(true);
-					// #TODO: the gets happen async and the set thermostat 
-					//        is called before the get calls return
-					GetCurrentTemperature();
-					GetDesiredTemperature();
-					ThermostatUI.SetThermostatMode("current-temp");
-				}
-			}
-			else {
-				var temp = Date.now() - Date.parse(lastHeardTime);
-				DebugLog("[Core Ping Successful] Core Offline: " + temp, 2);
-				ThermostatUI.SetOnline(false);
-			}
-		}
-		else {
-			ErrorLog("[Core Ping Successful] Data does not have last_heard.");
-			console.log(response);
-		}
-	};
-
-	function IsCoreOnline() {
-		var url = SparkBaseUrl;
-		$.ajax({
-			type: 'GET',
-			url: url,
-			data: { access_token: SPARK_ACCESS_TOKEN },
-			dataType: 'json',
-			success: function(data){
-				CorePingSuccessful(data);
-			},
-			error: function(){
-				ErrorLog("[Is Core Online] Api call failed.");
-			}
-		});
-
-	};
 
 	function GetCurrentTemperature() {
 		var url = SparkBaseUrl + '/' + SPARK_CORE_ID + '/' + 'CurrTemp' ;
@@ -89,12 +32,22 @@ $(function () {
 			dataType: 'json',
 			success: function(data){
 				var temp = data.result;
-				ThermostatUI.SetCurrentTemp(temp);
 				DebugLog("Current temp: " + temp, 2);
+				if (ThermostatUI.GetOnline() == false) {
+					ThermostatUI.SetOnline(true);
+					ThermostatUI.SetCurrentTemp(temp);
+					ThermostatUI.SetThermostatMode('current-temp');
+				}
+				else {
+					ThermostatUI.SetCurrentTemp(temp);
+				}
 			},
 			error: function(){
-				ErrorLog("[Get Current Temperature] Api call failed.");
-			}
+				ThermostatUI.SetOnline(false);
+				ThermostatUI.VisualizeDisabled();
+				ErrorLog("[Get Current Temperature] Api call failed. Core Offline.");
+			}, 
+			timeout: 2000
 		});
 
 	};
